54a37b30159b71fa505b4bcaf2dad8148c2084e6
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-07 10:59:05 +0100

    created custom user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    follows = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        verbose_name='suit',
+        through='social.UserFollows'
+    )
+
+    class Meta:
+        verbose_name = 'Utilisateur'
+        verbose_name_plural = 'Utilisateurs'
+
+    def __str__(self):
+        return self.username
