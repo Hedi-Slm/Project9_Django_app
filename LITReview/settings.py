3ece76a0090de613fecf091d5cff3f5c48583dd3
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-07 10:56:56 +0100

    added AUTH_USER_MODEL to settings

--- LITReview/settings.py
+++ LITReview/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'social',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
