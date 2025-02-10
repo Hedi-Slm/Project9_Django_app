679eb547010f736075326fcdc7d787520f75eb1e
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-10 13:58:16 +0100

    temp views for authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login_view(request):
+    return render(request, "authentication/login.html")
+
+
+def register_view(request):
+    return render(request, "authentication/register.html")
