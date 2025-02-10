edc36d39e75b01dd5c87974200b6df01db04e39d
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-10 15:49:40 +0100

    enable view of User table in admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username',)
+    search_fields = ('username',)
+    ordering = ('username',)
