3fa58c644103913ecfdbd4137f4b619bae4c20c3
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-10 15:55:04 +0100

    updated urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from .views import register_view, login_view, logout_view
 
 app_name = 'authentication'
+
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
