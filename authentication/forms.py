69d0cfeb7a8541dc647403dddc09e291b6cbaf92
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-10 15:50:53 +0100

    Created register form

---
+++ authentication/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()  # Get the custom User model
+
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-field', 'placeholder': "Nom d'utilisateur"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-field', 'placeholder': "Mot de passe"})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-field', 'placeholder': "Confirmer mot de passe"})
+    )
+
+    class Meta:
+        model = User  # Use the custom User model
+        fields = ['username', 'password1', 'password2']
