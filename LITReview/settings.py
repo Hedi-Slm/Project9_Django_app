7f621f66a791a9454b20ea6d14cc9289a3fa0548
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-12 21:38:07 +0100

    Added MEDIA_URL and MEDIA_ROOT to settings

--- LITReview/settings.py
+++ LITReview/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
