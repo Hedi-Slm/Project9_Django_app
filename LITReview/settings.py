c8b77436b3563eda4e633e718d8a1250302bca48
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-20 11:23:32 +0100

    Added LOGIN_URL

--- LITReview/settings.py
+++ LITReview/settings.py
@@ -128,6 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User'
 
+LOGIN_URL = 'authentication:login'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
