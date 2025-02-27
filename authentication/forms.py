18b784e26b3f70425529bee88eece52b28bde88b
Hedi-Slm <hedi.slm42@gmail.com>
2025-02-27 22:04:28 +0100

    created a custom AuthenticationForm for the login page

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 User = get_user_model()  # Get the custom User model
 
@@ -20,3 +20,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User  # Use the custom User model
         fields = ['username', 'password1', 'password2']
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-field', 'placeholder': "Nom d'utilisateur"})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-field', 'placeholder': "Mot de passe"})
+    )
